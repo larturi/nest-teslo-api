d2baae6fc2906ffafcf590226dc84387da9c91fa
Leandro Arturi <lea.arturi@gmail.com>
2022-08-30 17:23:26 -0300

    TypeORM & Postgres

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,22 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot(),
+
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: Number(process.env.DB_PORT),
+      database: process.env.DB_NAME,
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      autoLoadEntities: Boolean(process.env.DB_AUTOLOAD_ENTITIES),
+      synchronize:  Boolean(process.env.DB_SYNCHRONIZE),
+    }),
+  ],
   controllers: [],
   providers: [],
 })

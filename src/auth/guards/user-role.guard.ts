ad2ff6e04932f5f1fd051732ef7082568cf118c6
Leandro Arturi <lea.arturi@gmail.com>
2022-09-15 15:21:46 -0300

    Auth en Seed

--- src/auth/guards/user-role.guard.ts
+++ src/auth/guards/user-role.guard.ts
@@ -6,7 +6,7 @@ import { META_ROLES } from '../decorators/role-protected.decorator';
 
 @Injectable()
 export class UserRoleGuard implements CanActivate {
-
+  
   constructor(
     private readonly reflector: Reflector
   ) {}
@@ -14,21 +14,26 @@ export class UserRoleGuard implements CanActivate {
   canActivate(
     context: ExecutionContext,
   ): boolean | Promise<boolean> | Observable<boolean> {
+    
+    const validRoles: string[] = this.reflector.get( META_ROLES , context.getHandler() )
 
-    const validRoles: string[] = this.reflector.get(META_ROLES, context.getHandler());
-
+    if ( !validRoles ) return true;
+    if ( validRoles.length === 0 ) return true;
+    
     const req = context.switchToHttp().getRequest();
     const user = req.user as User;
 
-    if(!user)
+    if ( !user ) 
       throw new BadRequestException('User not found');
-
-    for(const role of user.roles) {
-      if(validRoles.includes(role)) {
+    
+    for (const role of user.roles ) {
+      if ( validRoles.includes( role ) ) {
         return true;
       }
     }
-
-    throw new ForbiddenException(`User ${user.fullName} is not allowed. Allowed roles: [${validRoles}]`);
+    
+    throw new ForbiddenException(
+      `User ${ user.fullName } need a valid role: [${ validRoles }]`
+    );
   }
 }

e72e63a9ea644de246adc92977843bf0cec8dace
Leandro Arturi <lea.arturi@gmail.com>
2022-09-16 12:05:21 -0300

    Inicio message service con socket.io

--- src/app.module.ts
+++ src/app.module.ts
@@ -10,6 +10,7 @@ import { CommonModule } from './common/common.module';
 import { SeedModule } from './seed/seed.module';
 import { FilesModule } from './files/files.module';
 import { AuthModule } from './auth/auth.module';
+import { MessageWsModule } from './message-ws/message-ws.module';
 
 @Module({
   imports: [
@@ -41,6 +42,8 @@ import { AuthModule } from './auth/auth.module';
     FilesModule,
 
     AuthModule,
+
+    MessageWsModule,
   ],
   controllers: [],
   providers: [],

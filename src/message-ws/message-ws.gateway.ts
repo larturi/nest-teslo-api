7e743351f61dc4cfd4090484e77688afdb00de4f
Leandro Arturi <lea.arturi@gmail.com>
2022-09-16 15:14:48 -0300

    Avances socket

--- src/message-ws/message-ws.gateway.ts
+++ src/message-ws/message-ws.gateway.ts
@@ -1,7 +1,20 @@
-import { WebSocketGateway } from '@nestjs/websockets';
+import { OnGatewayConnection, OnGatewayDisconnect, WebSocketGateway } from '@nestjs/websockets';
+import { Socket } from 'socket.io';
 import { MessageWsService } from './message-ws.service';
 
 @WebSocketGateway({ cors: true })
-export class MessageWsGateway {
-  constructor(private readonly messageWsService: MessageWsService) {}
+export class MessageWsGateway implements OnGatewayConnection, OnGatewayDisconnect {
+
+  constructor(
+    private readonly messageWsService: MessageWsService
+  ) {}
+  
+  handleDisconnect(client: Socket) {
+    this.messageWsService.registerClient(client);
+    console.log(`Number of clients: ${this.messageWsService.getConnectedClients()}`);
+  }
+  
+  handleConnection(client: Socket) {
+    this.messageWsService.removeClient(client.id);
+  }
 }
